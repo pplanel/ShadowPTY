25ffc65bda1d7da0200535b5261455b78117f6f1
Pedro Gabriel <pplanel@gmail.com>
2026-09-21 20:58:41 -0300

    build(nix): add asciinema to devShell packages

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
             rust-analyzer
             clippy
             cargo
+            asciinema
           ]
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             pkgs.apple-sdk_14
